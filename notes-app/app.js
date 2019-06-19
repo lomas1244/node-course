6b4c488656cb2a91f2097b4e098c24ced33f1671
James Lomas <jameslomas2004@gmail.com>
2019-06-19 14:11:32 +0100

    adding argument parsing for process argv index

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,24 +2,32 @@ const validator = require('validator')
 const chalk = require('chalk')
 const getNotes = require('./notes')
 
-const msg = getNotes()
-console.log(msg)
+const command = process.argv[2]
+const invalidArg = chalk.red('Please enter valid command')
 
-const successMsg = chalk.green('Success')
-const errorMsg = chalk.red('Error')
-
-console.log(successMsg)
-console.log(errorMsg)
-
-
-
-
+if (command === 'add') {
+    console.log('Adding note!')
+} else if (command === 'remove') {
+    console.log('Removing note!')
+} else {
+    console.log(invalidArg)
+}
 
 
 
 
 
 
+// const msg = getNotes()
+// console.log(msg)
+
+// const successMsg = chalk.green('Success')
+// const errorMsg = chalk.red('Error')
+
+// console.log(successMsg)
+// console.log(errorMsg)
+
+// console.log(process.argv[2])
 
 // console.log(validator.isURL('www.example.com'))
 // const add = require('./utils')
