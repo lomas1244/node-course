407da140f7077805e508080684e26636d177549f
James Lomas <jameslomas2004@gmail.com>
2019-06-23 15:40:26 +0100

    Beginning of section 19 creating the adding note functionality

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,69 +1,62 @@
-const validator = require('validator')
-const chalk = require('chalk')
-const getNotes = require('./notes')
-const yargs = require('yargs')
-const fs = require('fs')
+const validator = require("validator");
+const chalk = require("chalk");
+const notes = require("./notes");
+const yargs = require("yargs");
+const fs = require("fs");
 
 // Customise yargs version
-yargs.version('1.1.0')
+yargs.version("1.1.0");
 
 // Create 'add' command
 yargs.command({
-    command: 'add',
-    describe: 'Add a new note',
-    builder: {
-        title: {
-            describe: 'Note title',
-            demandOption: true,
-            type: 'string'
-        },
-        body: {
-            describe: 'Note body',
-            demandOption: true,
-            type: 'string'
-        }
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string"
     },
-    handler: function (props) {
-        console.log(chalk.yellow('Title: ' + props.title))
-        console.log(chalk.yellow('Body: ' + props.body))
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string"
     }
-})
+  },
+  handler: function(props) {
+    notes.addNote(props.title, props.body);
+  }
+});
 
 // Create 'remove' command
 yargs.command({
-    command: 'remove',
-    describe: 'Remove a note',
-    handler: function () {
-        console.log(chalk.red('Removing existing note...'))
-    }
-})
+  command: "remove",
+  describe: "Remove a note",
+  handler: function() {
+    console.log(chalk.red("Removing existing note..."));
+  }
+});
 
 // Create 'list' command
 yargs.command({
-    command: 'list',
-    describe: 'List all notes',
-    handler: function () {
-        console.log(chalk.blue('Listing all notes...'))
-    }
-})
+  command: "list",
+  describe: "List all notes",
+  handler: function() {
+    console.log(chalk.blue("Listing all notes..."));
+  }
+});
 
 // Create 'read' command
 yargs.command({
-    command: 'read',
-    describe: 'Read this note',
-    handler: function () {
-        console.log(chalk.yellow('Reading this note...'))
-    }
-})
+  command: "read",
+  describe: "Read this note",
+  handler: function() {
+    console.log(chalk.yellow("Reading this note..."));
+  }
+});
 
-
-console.log(yargs.parse())
+console.log(yargs.parse());
 // console.log(yargs.argv)
-
-
-
-
-
 
 // const command = process.argv[2]
 // const invalidArg = chalk.red('Please enter valid command')
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,23 @@
-const getNotes = function () {
-    return 'Your notes...'
-}
+const fs = require("fs");
 
-module.exports = getNotes
+const getNotes = function() {
+  return "Your notes...";
+};
+
+const addNote = function(title, body) {
+  const notes = loadNotes();
+};
+
+const loadNotes = function() {
+  try {
+  } catch (e) {}
+
+  const dataBuffer = fs.readFileSync("notes.json");
+  const dataJSON = dataBuffer.toString();
+  return JSON.parse(dataJSON);
+};
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote
+};
