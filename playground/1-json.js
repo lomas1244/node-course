ecfd11f539b1fc9fcd7b8bf2a709f54ee9d2b1fe
James Lomas <jameslomas2004@gmail.com>
2019-06-23 14:17:47 +0100

    editing json with file system commands

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,13 +1,26 @@
-const fs = require('fs')
+const fs = require("fs");
 // const book = {
-//     title: 'Ego is the enemny',
+//     title: 'Ego is the enemy',
 //     author: 'Ryan Holiday'
 // }
 
 // const bookJSON = JSON.stringify(book)
 // fs.writeFileSync('1-json.json', bookJSON)
 
-const dataBuffer = fs.readFileSync('1-json.json')
-const dataJSON = dataBuffer.toString()
-const data = JSON.parse(dataJSON)
-console.log(data.title)
+// const dataBuffer = fs.readFileSync("1-json.json");
+// const dataJSON = dataBuffer.toString();
+// const data = JSON.parse(dataJSON);
+// console.log(data.title);
+
+// Load the data
+const dataBuffer = fs.readFileSync("1-json.json");
+const dataJSON = dataBuffer.toString();
+// Parse the data
+const user = JSON.parse(dataJSON);
+// Update the current info
+user.name = "James";
+user.planet = "Titan";
+user.age = 24;
+// Stringify and update the original
+const userJSON = JSON.stringify(user);
+fs.writeFileSync("1-json.json", userJSON);
